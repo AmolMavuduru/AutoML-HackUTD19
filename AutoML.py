18611aa64876104d0d5b9553250f890214ce7c38
PratikBhusal <PratikBhusal@users.noreply.github.com>
2019-11-10 01:04:15 -0600

    Upgraded instance to better fit memory requirements

--- AutoML.py
+++ AutoML.py
@@ -15,9 +15,9 @@ from xgboost import XGBRegressor, XGBClassifier
 import matplotlib.pyplot as plt
 
 class AutoMLEstimator(object):
-    
+
     def __init__(self, **kwargs):
-        
+
         self.task = kwargs['task']
         self.speed = kwargs['speed']
         self.test_size = kwargs['test_size']
@@ -25,31 +25,31 @@ class AutoMLEstimator(object):
             self.tpot_model = TPOTClassifier(generations=self.speed, population_size=self.speed*5, verbosity=2, n_jobs=-1)
         else:
             self.tpot_model = TPOTRegressor(generations=self.speed, population_size=self.speed*5, verbosity=2, n_jobs=-1)
-        
+
     def preprocess_data(self, data, target_column):
-        
+
         clean_data = autoclean(data)
         X = clean_data.drop(target_column, axis=1)
         y = clean_data[target_column]
-        
+
         return X, y
-    
+
     def split_data(self, X, y):
-        
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=self.test_size) 
-    
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=self.test_size)
+
     def fit_model(self):
-        
+
         self.tpot_model.fit(self.X_train, self.y_train)
-        
+
     def run_automl(self, data, target_column):
-        
+
         self.X, self.y = self.preprocess_data(data, target_column)
         self.split_data(self.X, self.y)
         self.fit_model()
 
     def get_feature_importances(self):
-    
+
 	    img = io.BytesIO()
 
 	    if self.task == 'Classification':
@@ -60,7 +60,7 @@ class AutoMLEstimator(object):
 	    model.fit(self.X, self.y)
 	    importances = model.feature_importances_
 
-	    plt.figure(figsize=(12s, 6))
+	    plt.figure(figsize=(12, 6))
 	    plt.title('Most Important Features')
 	    plt.xlabel('Feature')
 	    plt.ylabel('Importance')
@@ -75,26 +75,26 @@ class AutoMLEstimator(object):
 	    plt.close()
 	    return 'data:image/png;base64,{}'.format(graph_url)
 
-    
+
     def evaluate_model(self):
-        
+
         metrics = defaultdict()
         pred = self.tpot_model.fitted_pipeline_.predict(self.X_test)
-        
+
         if self.task == 'Classification':
-            
+
             metrics['task'] = 'Classification'
             metrics['accuracy'] = int(100*accuracy_score(pred, self.y_test))
             metrics['precision'] = int(100*precision_score(pred, self.y_test, average="weighted"))
             metrics['recall'] = int(100*recall_score(pred, self.y_test, average="weighted"))
-            
+
         else:
-            
+
             metrics['task'] = 'Regression'
             metrics['r2_score'] = r2_score(pred, self.y_test)
             metrics['mean_absolute_error'] = mean_absolute_error(pred, self.y_test)
             metrics['mean_squared_error'] = mean_squared_error(pred, self.y_test)
-        
+
         metrics['model_name'] = type(self.tpot_model.fitted_pipeline_[-1]).__name__
         metrics['feat_importances'] = self.get_feature_importances()
         return metrics
